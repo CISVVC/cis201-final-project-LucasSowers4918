e987c8e71a946183e22fb5a451d7050cdb8af208
lucas sowers <lucassowers@cis.vvc.edu>
2018-12-15 18:49:05 -0800

    update

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
-#include<iostream>
 
-using namespace std;
+/*
+file: main.cpp
+Description: 
+Author: Lucas Sowers
+Email: lucass4918@student.vvc.edu
+Date Created: 12/15/18
+*/	
+#include "concordance.h"
 
 int main()
-{
-   return 0;
-}
+	{
+	   Concordance con("alice.txt");
+	   con.parse();   
+	   con.print();
+	
+
+	   return 0;
+	}
+
